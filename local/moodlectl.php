a04bdc284c6608fa7e9b0a9af8e772145d349bae
Bart <bart@dataview.co.nz>
2011-02-12 01:32:28 +1300

    bonsai to laptop test

--- local/moodlectl.php
+++ local/moodlectl.php
@@ -58,9 +58,8 @@
  *                     // repeat for each action
  *                    );
  */
-// set error reporting for debugging
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED); // these error settings seem to make no difference, and neither do the cli/php.ini settings.. why?
-ini_set('display_errors', '1');
+
+// Added on bonsai
 
 // specifies which directory moodlectl lives in, in relation to dirroot
 define('MOODLECTL_BASE', 'local');
